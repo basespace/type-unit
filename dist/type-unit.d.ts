06d203246a78c68f9d07d8cce5033bf7fd5f4269
aroberts-illumina <aroberts@illumina.com>
2015-09-09 14:39:28 -0700

    Update type-unit.d.ts file with isAsync params

--- dist/type-unit.d.ts
+++ dist/type-unit.d.ts
@@ -1,5 +1,5 @@
 declare module "type-unit" {
 	export var Suite: (description: string) => (target: any) => void;
-    export var Fact: (description: string, id?: string) => (target: Object, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) => void;
-    export var Theory: (description: string, parameters: any[][]) => (target: Object, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) => void;
+    export var Fact: (description: string, isAsync?: boolean) => (target: Object, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) => void;
+    export var Theory: (description: string, parameters: any[][], isAsync?: boolean) => (target: Object, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) => void;
 }
