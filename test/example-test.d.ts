bf9cdf1c81376b0f1f0a2d1903650ec74d1f1a3f
aroberts-illumina <aroberts@illumina.com>
2016-04-21 23:32:26 -0700

    Remove test folder

--- test/example-test.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="../typings/node/node.d.ts" />
-/// <reference path="../typings/mocha/mocha.d.ts" />
-declare var path: any, spawn: any, assert: any;
-declare function assertContains(needle: string, haystack: string): void;
